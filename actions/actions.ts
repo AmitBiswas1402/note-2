a939896eec2eddd15446c0c40e92b9c8a5d66240
Amit Biswas <amit.142biswas@gmail.com>
2024-08-06 20:43:07 +0530

    boring

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,9 +1,22 @@
 'use server'
 
+import { adminDb } from "@/firebase-admin"
 import { auth } from "@clerk/nextjs/server"
 
 export async function createdDocuemt() {
     auth().protect()
 
     const { sessionClaims } = await auth()
+
+    const docCollectionRef = adminDb.collection('docuemnt')
+    const docRef = await docCollectionRef.add({
+        title: "New Doc"
+    })
+
+    await adminDb.collection('users').doc(sessionClaims?.email!).collection('rooms').doc(docRef.id).set({
+        userId: sessionClaims?.email,
+        role: "owner",
+        createdAt: new Date(),
+        rowid: docRef.id
+    })
 }
